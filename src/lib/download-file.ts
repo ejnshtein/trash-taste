18ea7bd541693a02bccecf2081ce9ac53ca6dd4c
Aleksandrs Rivkinds <ejnshtein@gmail.com>
2024-09-13 23:59:50 +0300

    reduce the number of connections for download

--- src/lib/download-file.ts
+++ src/lib/download-file.ts
@@ -5,7 +5,7 @@ export const downloadFile = async (
   filePath: string
 ): Promise<void> => {
   await new EasyDl(url, filePath, {
-    connections: 10,
+    connections: 5,
     maxRetry: 5,
     existBehavior: 'overwrite'
   }).wait()
